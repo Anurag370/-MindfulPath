98251fc2af635a9ae6d9f7fbf0da301a6abfe0e3
Dhrubajyoti Nath <dhrubajyotinath371@gmail.com>
2026-01-04 21:16:54 +0530

    front done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
+
 import React from 'react'
 import Navbar from './Pages/Navbar'
 //import HomeSection from './Pages/HomeSection'
 import ChatPage from './ChatPage'
+import HomeSection from './Pages/HomeSection'
 const App = () => {
   return (
     <div>
        <Navbar />
-       
+<HomeSection/>
         <ChatPage/>
 
 </div>
