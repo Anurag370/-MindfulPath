ef565f8ec6d57a7002b120073738a90901dc9c65
Anurag Hom Roy <homroyanurag@gmail.com>
2026-01-05 08:03:39 +0530

    Ready for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 
-import Navbar from "./component/Navbar.jsx";
-import ChatPage from "./Pages/ChatPage.jsx";
-import HomeSection from "./Pages/HomeSection.jsx";
+import Navbar from "./component/Navbar";
+import ChatPage from "./Pages/ChatPage";
+import HomeSection from "./Pages/HomeSection";
 const App = () => {
   return (
     <div>
