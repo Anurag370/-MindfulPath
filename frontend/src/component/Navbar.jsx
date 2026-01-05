8cd780d16828653cb4912054c236a58dca2f3569
Anurag Hom Roy <homroyanurag@gmail.com>
2026-01-05 08:10:35 +0530

    Ready for deployment

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -1,17 +1,19 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <header className="flex items-center justify-between px-8 py-4 bg-white shadow-sm">
 
-      <div className="flex items-center gap-2 text-xl font-semibold text-emerald-600"><a href="/">
+      <div className="flex items-center gap-2 text-xl font-semibold text-emerald-600"><Link to="/">
         <span className="text-2xl">🌿</span>
-        <span className="text-gray-900">MindfulPath</span></a>
+        <span className="text-gray-900">MindfulPath</span></Link>
       </div>
 
 
       <nav className="flex gap-6 text-sm font-medium text-gray-700">
-        <a href="/chat" className="hover:text-emerald-600">
+        <Link to ="/chat" className="hover:text-emerald-600">
           Chat
-        </a>
+        </Link>
         <a href="#mood" className="hover:text-emerald-600">
           Mood Tracker
         </a>
