ef2c40ca9b298e6ce93e55797a04066402b9c827
Anurag Hom Roy <homroyanurag@gmail.com>
2026-01-09 20:38:00 +0530

    ready for deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="MindfulPath")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","https://mindfulpathcod.onrender.com/chat"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- frontend/src/Pages/ChatPage.jsx
+++ frontend/src/Pages/ChatPage.jsx
@@ -13,7 +13,7 @@ export default function ChatPage() {
 
         setInput("");
         try{
-            const res = await fetch("http://127.0.0.1:8000/chat",{
+            const res = await fetch("https://mindfulpathcodbackend.onrender.com/chat",{
                 method : "POST",
                 headers: {"Content-Type":"application/json"},
                 body: JSON.stringify({message:input})
